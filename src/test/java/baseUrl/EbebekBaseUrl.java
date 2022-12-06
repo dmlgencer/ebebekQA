dffa224df81a697e2a52b027ac6cbc0375d0e542
Damla Gencer <damla.45gencer@gmail.com>
2022-12-06 12:32:34 +0300

    ebebekQA

--- src/test/java/baseUrl/EbebekBaseUrl.java
+++ src/test/java/baseUrl/EbebekBaseUrl.java
@@ -1,10 +1,23 @@
 package baseUrl;
 
+import io.cucumber.java.AfterAll;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import org.apache.poi.ss.formula.functions.T;
 import org.junit.Before;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import testResultLogger.TestResultLogger;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(TestResultLogger.class)
 
 public class EbebekBaseUrl {
+
     protected RequestSpecification spec;
     //If you use @Before annotation at the top of a method, it will be executed before every test method.
     @Before
@@ -13,4 +26,5 @@ public class EbebekBaseUrl {
         spec = new RequestSpecBuilder().setBaseUri("https://www.e-bebek.com/").build();
 
     }
+
 }
